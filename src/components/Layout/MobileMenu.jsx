ca0ef42fee11418059295589644eca956fea8a01
Hasan Talha Çelik <35665454+bitterkofte@users.noreply.github.com>
2024-11-19 01:17:30 +0300

    mobil nav language

--- src/components/Layout/MobileMenu.jsx
+++ src/components/Layout/MobileMenu.jsx
@@ -1,44 +1,51 @@
+import { useTranslation } from "react-i18next";
 import MyLink from "../Elements/MyLink";
 
 const MobileMenu = () => {
+  const { t } = useTranslation();
   return (
-    <div className="fixed top-0 left-0 px-20 w-full h-screen flex flex-col justify-center gap-8 animate-mobileMenuDown animate-mobileMenuUp bg-lp-brown-d2 text-white">
+    <div className="fixed top-0 left-0 px-20 pt-20 w-full h-screen flex flex-col gap-3  animate-mobileMenuDown animate-mobileMenuUp bg-lp-brown-d2 text-white">
       {/* SECTION 1st Col */}
       <div className="max-w-52">
-        <h3 className="text-lg font-bold">Kurumsal</h3>
+        <h3 className="text-lg font-bold">{t("navbar.kurumsal")}</h3>
         <div className="ml-3 flex flex-col gap-2">
-          <MyLink to={"/"}>Anasayfa</MyLink>
-          <MyLink to={"/hakkimizda"}>Hakkımıda</MyLink>
+          <MyLink to={"/"}>{t("navbar.anasayfa")}</MyLink>
+          <MyLink to={"/hakkimizda"}>{t("navbar.hakkimizda")}</MyLink>
           <MyLink to={"/neden-petek-ve-leblebici"}>
-            Neden Petek & Leblebici
+            {t("navbar.nedenPL")}
           </MyLink>
-          <MyLink to={"/sikca-sorulan-sorular"}>Sıkça Sorulan Sorular</MyLink>
+          <MyLink to={"/sikca-sorulan-sorular"}>{t("navbar.sss")}</MyLink>
         </div>
       </div>
 
       {/* SECTION 2nd Col */}
       <div className="max-w-52 content-start">
-        <h3 className="text-lg font-bold">Hukuk</h3>
+        <h3 className="text-lg font-bold">{t("navbar.hukuk")}</h3>
         <div className="ml-3 flex flex-col gap-2">
-          <MyLink to={"/sigorta-hukuku"}>Sigorta Hukuku</MyLink>
-          <MyLink to={"/hukuki-danismanlik"}>Hukuki Danışmanlık</MyLink>
+          <MyLink to={"/sigorta-hukuku"}>{t("navbar.sigortaH")}</MyLink>
+          <MyLink to={"/hukuki-danismanlik"}>{t("navbar.hukukiD")}</MyLink>
         </div>
       </div>
 
       {/* SECTION 3rd Col */}
       <div className="max-w-52">
-        <h3 className="text-lg font-bold">Hizmetler</h3>
+        <h3 className="text-lg font-bold">{t("navbar.hizmetler")}</h3>
         <div className="ml-3 flex flex-col gap-2">
           <MyLink to={"/onleyici-hukuk-hizmetleri"}>
-            Önleyici Hukuk Hizmetleri
+            {t("navbar.onleyiciHH")}
           </MyLink>
           <MyLink to={"/uzlastirma-ve-arabuluculuk-hizmetleri"}>
-            Uzlaştırma ve Arabuluculuk Hizmetleri
+            {t("navbar.uzlastirmaAH")}
           </MyLink>
           <MyLink to={"/kvkk-ve-gdpr-hukuki-danismanlik"}>
-            KVKK ve GDPR Hukuki Danışmanlık
+            {t("navbar.kGHD")}
           </MyLink>
         </div>
+      </div>
+
+      {/* SECTION 4th Col */}
+      <div className="max-w-52 text-white font-bold">
+        <MyLink to={"/iletisim"}>{t("navbar.iletisim")}</MyLink>
       </div>
     </div>
   );
